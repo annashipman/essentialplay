34999a7cbbc7da21130be7bc765d046e6d17ad46
Anna Shipman <anna@annashipman.co.uk>
2014-10-02 10:48:59 +0100

    Run the app
    And correct some typos to make it work

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -7,7 +7,7 @@ object ChatController extends Controller {
     //Place your actions here
 
     def index = Action { request =>
-      Ok("hello world) //will construct a response with a 200
+      Ok("hello world") //will construct a response with a 200
     }
 
 }
