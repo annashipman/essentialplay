1df5d5aae91d7d72f704edc191aea1ee900c0437
Anna Shipman <anna@annashipman.co.uk>
2014-10-02 11:26:15 +0100

    Add an action that posts a message

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -4,10 +4,15 @@ import play.api.mvc._
 
 object ChatController extends Controller {
 
-    val message = Seq("Hello", "This is", "Awesome")
+    var message = Seq("Hello", "This is", "Awesome")
 
     def index = Action { request =>
       Ok(views.html.index(message)) 
+    }
+
+    def postMessage(newBit: String) = Action { request =>
+      message = message :+ newBit
+      Ok(views.html.index(message))
     }
 
     def calc(a: String, b: String) = Action { request =>
