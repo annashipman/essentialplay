e70b33aad31f6a364f91eed9468d341c0a8c09e2
Anna Shipman <anna@annashipman.co.uk>
2014-10-02 10:29:11 +0100

    A constructor call basically

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -1,3 +1,13 @@
 package controllers
 
 import play.api.mvc._
+
+object ChatController extends Controller {
+
+    //Place your actions here
+
+    def index = Action { request =>
+      Ok("hello world) //will construct a response with a 200
+    }
+
+}
