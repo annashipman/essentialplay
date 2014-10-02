cfc3459714f426b5bc215e86dfaf1950b7856235
Anna Shipman <anna@annashipman.co.uk>
2014-10-02 11:06:15 +0100

    Use a view

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 object ChatController extends Controller {
 
     def index = Action { request =>
-      Ok("hello world") //will construct a response with a 200
+      Ok(views.html.index()) 
     }
 
     def calc(a: String, b: String) = Action { request =>
